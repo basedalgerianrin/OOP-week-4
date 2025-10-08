070cd89277a78638eaecb593ba3964cc03df3724
Sami Hammar <G00393324@atu.ie>
2025-10-08 17:43:46 +0100

    Main Push

--- src/main/java/ie/atu/week4/Main.java
+++ src/main/java/ie/atu/week4/Main.java
@@ -1,19 +1,48 @@
 package ie.atu.week4;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        List<Student> studentsList = new ArrayList<Student>();
+        Student s1 = new Student();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Enter number of students: ");
+        int total = sc.nextInt();
+        sc.nextInt();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        while (total > i) {
+
+            System.out.println("Enter Name: ");
+            String name = sc.nextLine();
+            System.out.println("Enter Email: ");
+            String email = sc.nextLine();
+            System.out.println("Enter Course: ");
+            String course = sc.nextLine();
+            System.out.println("Enter Student ID: ");
+            String sid = sc.nextLine();
+
+            Student s2 = new Student();
+            s1.setName(name);
+            s1.setEmail(email);
+            s1.setCourse(course);
+            s1.setID(sid);
+            studentsList.add(s1);
+
+            i++;
         }
+
+        for(Student student:studentsList){
+            System.out.println("Name: " + s1.getName() + "\nEmail: " + s1.getEmail() + "\nID: " + s1.getID() + "\nCourse: " + s1.getCourse());
+        }
+
+
     }
 }
---
+++ src/main/java/ie/atu/week4/Student.java
@@ -0,0 +1,37 @@
+package ie.atu.week4;
+
+public class Student {
+private String Name, Email, ID, Course;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getCourse() {
+        return Course;
+    }
+
+    public void setCourse(String course) {
+        Course = course;
+    }
+}
